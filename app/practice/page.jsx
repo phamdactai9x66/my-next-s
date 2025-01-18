ddb25ee5dbfd4429ba751a218ac92bf762107c05
tai.pham <tai15122003311@gmail.com>
2025-01-18 12:03:29 +0700

    first practice

---
+++ app/practice/page.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Practice = () => {
+  return (
+    <div className="container">
+      <div className="flex gap-4 p-2 justify-center">
+        <span className="btn bg-gray-50 text-gray-500 ">Badge</span>
+
+        <span className="btn bg-red-50 text-red-500  ">Badge</span>
+
+        <span className="btn bg-yellow-50 text-yellow-500  ">Badge</span>
+      </div>
+    </div>
+  );
+};
+
+export default Practice;
